7e18ef047b6921a7510fcca081dfe4e683e12e77
CHLEE0123 <lchenghong1113@gmail.com>
2023-12-31 09:34:13 +0800

    pad snap try

--- Assets/AED_Event.cs
+++ Assets/AED_Event.cs
@@ -7,25 +7,60 @@ public class AED_Event : MonoBehaviour
     public GameObject MdBoxVisual;
     public GameObject AEDSet;
 
+    public GameObject PadUpVisual;
+    public GameObject PadDownVisual;
+
     // Start is called before the first frame update
     void Start()
     {
         MdBoxVisual.SetActive(false);
         AEDSet.SetActive(false);
-
+        PadUpVisual.SetActive(false);
+        PadDownVisual.SetActive(false);
     }
 
+    public void HideAED()
+    {
+        AEDSet.SetActive(false);
+
+    }
+    public void ShowAED()
+    {
+        AEDSet.SetActive(true);
+    }
 
     public void ShowMdBoxVisual()
     {
         MdBoxVisual.SetActive(true);
-        AEDSet.SetActive(false);
 
     }
 
     public void HideMdBoxVisual()
     {
         MdBoxVisual.SetActive(false);
-        AEDSet.SetActive(true);
+    }
+
+
+
+
+
+    public void Show_PadUp_Visual()
+    {
+        PadUpVisual.SetActive(true);      
+    }
+
+    public void Show_PadDown_Visual()
+    {
+        PadDownVisual.SetActive(true);
+    }
+
+    public void Hide_PadUp_Visual()
+    {
+        PadUpVisual.SetActive(false);
+    }
+
+    public void Hide_PadDown_Visual()
+    {
+        PadDownVisual.SetActive(false);
     }
 }
