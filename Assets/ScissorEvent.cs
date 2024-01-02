9bc00ab5ba75257c977254739535a3d70e6331b8
CHLEE0123 <lchenghong1113@gmail.com>
2024-01-02 10:40:48 +0800

    button shock

--- Assets/ScissorEvent.cs
+++ Assets/ScissorEvent.cs
@@ -25,4 +25,5 @@ public class ScissorEvent : MonoBehaviour
         }
     }
 
+
 }
--- Assets/aed_button.cs
+++ Assets/aed_button.cs
@@ -11,6 +11,12 @@ public class aed_button : MonoBehaviour
     public GameObject aedButton_UIPlace2;
     public Collider aed_collider;
 
+    public GameObject AEDold_UI;
+    public GameObject AEDnew_UI;
+
+    public GameObject startButton;
+    public GameObject shockButton;
+
     public TextMeshProUGUI aedUI_Text;
     public TextMeshProUGUI aedButton_Text;
 
@@ -20,6 +26,9 @@ public class aed_button : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        shockButton.SetActive(false);
+        startButton.SetActive(true);
+        AEDnew_UI.SetActive(false);
         aed_collider = GetComponent<Collider>();
     }
 
@@ -29,11 +38,15 @@ public class aed_button : MonoBehaviour
         {
             aedUI_Text.text = "Make sure NOBODY is TOUCHING the patient, Press on 'Shock' button";
             aedButton_Text.text = "Shock";
-            aed_collider.enabled = true;
+            //aed_collider.enabled = true;
+            aed_Handvisual.SetActive(true);
+            shockButton.SetActive(true);
             aed_Handvisual.transform.position = aed_Handvisual_shockPlace.transform.position;
             aed_Handvisual.transform.rotation = aed_Handvisual_shockPlace.transform.rotation;
 
             aedButton_UI.transform.position = aedButton_UIPlace2.transform.position;
+
+            
         }
 
     }
@@ -60,7 +73,9 @@ public class aed_button : MonoBehaviour
     {
         if (aed_collider != null)
         {
+            // startButton.SetActive(false);
             // Set the collider's enabled property to false
+            Debug.Log("Shock button pocked!");
             aed_collider.enabled = false;
             aed_Handvisual.SetActive(false);
             aedUI_Text.text = "First, Cut off patient's cloth with the scissor";
@@ -68,12 +83,23 @@ public class aed_button : MonoBehaviour
 
     }
 
-    public void shockButton_pock()
+    public void startButton_release()
     {
-            // Set the collider's enabled property to false
-            aed_collider.enabled = false;
-            aed_Handvisual.SetActive(false);
-            aedUI_Text.text = "Ready to shock, 3..2..1.. Shock!";
+        startButton.SetActive(false);
+    }
+
+    public void Shock_pock()
+    {
+        // Set the collider's enabled property to false
+        
+        Debug.Log("Shock button pocked!");
+        //aed_collider.enabled = false;
+        aed_Handvisual.SetActive(false);
+        //aedUI_Text.text = "Ready to shock, 3..2..1.. Shock!";
+        AEDold_UI.SetActive(false);
+        AEDnew_UI.SetActive(true);
+        
+        Debug.Log("Shock button pocked!DONE DONE DONE");
 
     }
 
